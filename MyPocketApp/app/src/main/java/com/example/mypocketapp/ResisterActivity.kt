8906d6a2c9ac7909e44898d90ae4075dd9897801
ShinDongeun <dongeun@pukyong.ac.kr>
2022-09-28 18:57:20 +0900

    from wap repo

---
+++ MyPocketApp/app/src/main/java/com/example/mypocketapp/ResisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mypocketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resister)
+    }
+}
